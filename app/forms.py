fb9f3eb9eba78b8822869a7206aa24ee074f296d
Manan Bhavsar <mananbh9@live.com>
2020-08-30 20:05:40 -0400

    corrected the layout of the blocks

--- app/forms.py
+++ app/forms.py
@@ -1,20 +1,17 @@
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import Email, InputRequired, Length
+from wtforms.widgets import TextArea
 from flask_wtf import FlaskForm
 import re
 
-def valid_email(form, field):
-	regex = r"^[a-z0-9-_]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
-	if not (re.search(field.data, regex)):
-		raise ValidationError('Please provide proper email address.') 
 
 class ContactForm(FlaskForm):
     f_name = StringField('First Name', [InputRequired(message="Please provide your First name.")])
     l_name = StringField('Last Name', [InputRequired(message="Please provide your First name.")])
     email = EmailField('Email', [InputRequired()])
     subject = StringField('Subject', [InputRequired(), Length(max="128", min="1", message="1 to 256 characters are required in this field.")])
-    message = StringField('Message', [InputRequired(), Length(min="16", message="16 characters or more are required in this field.")])
+    message = TextAreaField('Message', [InputRequired(), Length(min="16", message="16 characters or more are required in this field.")])
     submit = SubmitField('Send')
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from app.main import app 
   
 if __name__ == "__main__":
-	app.run(debug=False)
+	app.run(debug=True)
+
+
+# export FROM_EMAIL_ADDRESS=
+# export FROM_EMAIL_PASSSWORD=
+# export MYCV_WEB_APP_SECRET= 
